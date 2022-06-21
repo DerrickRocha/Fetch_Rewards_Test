5a940a9c54a1a7872272bf6f6987fc2fbe716fb4
derrick <drocha616@gmail.com>
2022-06-21 12:57:59 -0600

    Added RealHomeRepository and dependency creator class.

---
+++ app/src/main/java/com/example/fetchrewardstest/Dependencies.kt
@@ -0,0 +1,21 @@
+package com.example.fetchrewardstest
+
+import com.example.fetchrewardstest.network.FetchApiService
+import com.example.fetchrewardstest.repositories.HomeRepository
+import com.example.fetchrewardstest.repositories.RealHomeRepository
+import retrofit2.Retrofit
+
+object Dependencies {
+    private var initialized = false
+    lateinit var repository: HomeRepository
+
+    fun initialize() {
+        if (!initialized) {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://fetch-hiring.s3.amazonaws.com/")
+                .build()
+            val api = retrofit.create(FetchApiService::class.java)
+            repository = RealHomeRepository(api)
+        }
+    }
+}
--- app/src/main/java/com/example/fetchrewardstest/repositories/HomeRepository.kt
+++ app/src/main/java/com/example/fetchrewardstest/repositories/HomeRepository.kt
@@ -1,7 +1,12 @@
 package com.example.fetchrewardstest.repositories
 
 import com.example.fetchrewardstest.models.Item
+import com.example.fetchrewardstest.network.FetchApiService
 
 interface HomeRepository {
     suspend fun getItems(): List<Item>
+}
+
+class RealHomeRepository(private val service: FetchApiService): HomeRepository {
+    override suspend fun getItems(): List<Item> = service.getList()
 }
