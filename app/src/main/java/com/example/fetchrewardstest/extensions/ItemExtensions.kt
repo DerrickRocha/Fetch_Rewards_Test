17569eac77a458ff873702124cac533ca4b84899
derrick <drocha616@gmail.com>
2022-06-22 00:23:39 -0600

    Added sortByListIdThenByName. Added unit tests.

--- app/src/main/java/com/example/fetchrewardstest/extensions/ItemExtensions.kt
+++ app/src/main/java/com/example/fetchrewardstest/extensions/ItemExtensions.kt
@@ -2,10 +2,20 @@ package com.example.fetchrewardstest.extensions
 
 import com.example.fetchrewardstest.models.Item
 
-fun List<Item>.sortByListIdThenByName(): List<Item> = sortedBy { it.listId }.sortByName()
-
-fun List<Item>.sortByListId(): List<Item> = sortedBy { it.listId }
-
-fun List<Item>.sortByName(): List<Item> {
-    return emptyList()
+fun List<Item>.sortByListIdThenByName(): List<Item> {
+    return sortedWith(
+            compareBy(
+                { it.listId },
+                {
+                    val numberFromName = it.name?.filter { name -> name.isDigit() }
+                    if (numberFromName.isNullOrBlank()) {
+                        0
+                    } else {
+                        numberFromName.toInt()
+                    }
+                }
+            )
+        )
 }
+
+fun List<Item>.filterOutBlankAndNullNames(): List<Item> = filter { !it.name.isNullOrBlank() }
--- app/src/test/java/com/example/fetchrewardstest/extensions/ItemExtensionsTests.kt
+++ app/src/test/java/com/example/fetchrewardstest/extensions/ItemExtensionsTests.kt
@@ -1,28 +1,82 @@
 package com.example.fetchrewardstest.extensions
 
+import android.util.Log
 import com.example.fetchrewardstest.models.Item
 import org.junit.Test
 
 class ItemExtensionsTests {
 
     @Test
-    fun sortByListId_whenListIdsAreDifferent_listIdsShouldBeSorted() {
-        val unsorted = listOf(
-            Item(0,99, ""),
-            Item(0,47, ""),
+    fun filterOutBlankAndNullNames_whenListHasNullAndBlankNames_itemsWithNullAndBlankNamesShouldBeRemoved() {
+        val items = listOf(
+            Item(0,99, "Item 123"),
+            Item(0,47, "a"),
+            Item(0,1, "b"),
             Item(0,1, ""),
-            Item(0,1, ""),
-            Item(0,1009, ""),
+            Item(0,1, null),
+            Item(0,1, "Item 4"),
+            Item(0,1009, "d"),
             Item(0,0, ""),
-            Item(0,0, "")
+            Item(0,0, "f")
         )
-        val sorted = unsorted.sortByListId()
+        val filteredItems = items.filterOutBlankAndNullNames()
+        assert(filteredItems.size == 6)
+        assert(items[0].name == "Item 123")
+        assert(filteredItems[1].name == "a")
+        assert(filteredItems[2].name == "b")
+        assert(filteredItems[3].name == "Item 4")
+        assert(filteredItems[4].name == "d")
+        assert(filteredItems[5].name == "f")
+    }
+
+    @Test
+    fun sortByListIdThenByName_whenNamesAreNotValid_shouldBeSortedByListId() {
+        val unsorted = listOf(
+            Item(0,99, "Item 123"),
+            Item(0,47, "a"),
+            Item(0,1, "b"),
+            Item(0,1, null),
+            Item(0,1, null),
+            Item(0,1, "Item 4"),
+            Item(0,1009, "d"),
+            Item(0,0, "e"),
+            Item(0,0, "f")
+        )
+        val sorted = unsorted.sortByListIdThenByName()
         assert(sorted[0].listId == 0)
         assert(sorted[1].listId == 0)
         assert(sorted[2].listId == 1)
         assert(sorted[3].listId == 1)
-        assert(sorted[4].listId == 47)
-        assert(sorted[5].listId == 99)
-        assert(sorted[6].listId == 1009)
+        assert(sorted[4].listId == 1)
+        assert(sorted[5].listId == 1)
+        assert(sorted[6].listId == 47)
+        assert(sorted[7].listId == 99)
+        assert(sorted[8].listId == 1009)
+    }
+
+    @Test
+    fun sortByListIdThenByName_whenNamesAreValid_shouldBeSortedByListIdThenName() {
+        val unsorted = listOf(
+            Item(0,90, "Item 275"),
+            Item(0,605, "Item 72"),
+            Item(0,2, "Item 5"),
+            Item(0,605, "Item 0"),
+            Item(0,90, null),
+            Item(0,90, ""),
+            Item(0,90, "Item 10099"),
+            Item(0,8, "Item 5")
+        )
+        val sorted = unsorted.sortByListIdThenByName()
+        assert(sorted[0].listId == 2 && sorted[0].name == "Item 5")
+        assert(sorted[1].listId == 8 && sorted[1].name == "Item 5")
+        assert(sorted[2].listId == 90 && sorted[2].name == null)
+        assert(sorted[3].listId == 90 && sorted[3].name == "")
+        assert(sorted[4].listId == 90 && sorted[4].name == "Item 275")
+        assert(sorted[5].listId == 90 && sorted[5].name == "Item 10099")
+        assert(sorted[6].listId == 605 && sorted[6].name == "Item 0")
+        assert(sorted[7].listId == 605 && sorted[7].name == "Item 72")
+
+
+
     }
 }
