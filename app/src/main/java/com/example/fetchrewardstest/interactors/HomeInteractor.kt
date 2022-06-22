20d58f47b30a65e616e481257c396e0039d1928e
derrick <drocha616@gmail.com>
2022-06-22 11:42:50 -0600

    Added HomeInteractor.

--- app/src/main/java/com/example/fetchrewardstest/interactors/HomeInteractor.kt
+++ app/src/main/java/com/example/fetchrewardstest/interactors/HomeInteractor.kt
@@ -1,4 +1,18 @@
 package com.example.fetchrewardstest.interactors
 
+import com.example.fetchrewardstest.extensions.filterOutBlankAndNullNames
+import com.example.fetchrewardstest.extensions.sortByListIdThenByName
+import com.example.fetchrewardstest.models.Item
+import com.example.fetchrewardstest.repositories.HomeRepository
+
 interface HomeInteractor {
+    suspend fun getItems(): List<Item>
+}
+
+class RealHomeInteractor(private val repository: HomeRepository) : HomeInteractor {
+    override suspend fun getItems(): List<Item> {
+        return repository.getItems()
+            .filterOutBlankAndNullNames()
+            .sortByListIdThenByName()
+    }
 }
--- app/src/main/java/com/example/fetchrewardstest/repositories/HomeRepository.kt
+++ app/src/main/java/com/example/fetchrewardstest/repositories/HomeRepository.kt
@@ -4,9 +4,9 @@ import com.example.fetchrewardstest.models.Item
 import com.example.fetchrewardstest.network.FetchApiService
 
 interface HomeRepository {
-    suspend fun getItems(): List<Item>
+    fun getItems(): List<Item>
 }
 
 class RealHomeRepository(private val service: FetchApiService): HomeRepository {
-    override suspend fun getItems(): List<Item> = service.getList()
+    override fun getItems(): List<Item> = service.getList()
 }
